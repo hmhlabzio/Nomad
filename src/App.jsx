a5a22c662f94bdc2e627c3aabce68e5803e6a705
Santosh Saroj <santoshsaroj0032@gmail.com>
2025-06-05 18:47:16 +0530

    Details POP

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import LifestyleMeter from "./components/LifestyleMeter";
 function App() {
   return (
     <div className="bg-black text-white min-h-screen flex flex-col items-center">
-    <div className="w-full max-w-[1800px] px-4 py-6">
+    {/* <div className="w-full max-w-[1800px] px-4 py-6">w-full */}
+     <div className="w-full">
       <Hero />
       <CityPreview />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
